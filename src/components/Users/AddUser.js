2285417b6dd35f4d18566d1402c0d53312a228d7
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-02-15 17:08:46 +0530

    custom wrapper fragment created, real fragment is used and portal is created

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import classes from './AddUser.module.css';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper';
 
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
@@ -44,7 +45,7 @@ const AddUser = (props) => {
     }
 
     return (
-        <div>
+        <Wrapper>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
@@ -55,8 +56,7 @@ const AddUser = (props) => {
                     <Button type='submit'>Add User</Button>
                 </form>
             </Card>
-        </div>
-        
+        </Wrapper>
     );
 }
 
